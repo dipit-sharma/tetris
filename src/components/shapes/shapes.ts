5e96a61bb2005905ad56f5ee329268804077fd44
superstark02@gmail.com <superstark02@gmail.com>
2026-03-01 15:48:38 +0530

    added correct shapes

--- src/components/shapes/shapes.ts
+++ src/components/shapes/shapes.ts
@@ -1,24 +1,24 @@
 import type { TShapeArray, TShapeList } from "./types";
 
 export const RECTANGLE: TShapeArray = [
-    { x: 3, y: 0 },
-    { x: 4, y: 0 },
-    { x: 5, y: 1 },
-    { x: 6, y: 1 },
+    { x: 5, y: -1 },
+    { x: 6, y: -1 },
+    { x: 3, y: -1 },
+    { x: 4, y: -1 },
 ];
 
 export const SQUARE: TShapeArray = [
-    { x: 4, y: 0 },
-    { x: 5, y: 0 },
-    { x: 4, y: 1 },
-    { x: 5, y: 1 },
+    { x: 4, y: -2 },
+    { x: 5, y: -2 },
+    { x: 4, y: -1 },
+    { x: 5, y: -1 },
 ];
 
 export const T_SHAPE: TShapeArray = [
-    { x: 4, y: 0 },
-    { x: 3, y: 1 },
-    { x: 4, y: 1 },
-    { x: 5, y: 1 },
+    { x: 3, y: -2 },
+    { x: 4, y: -2 },
+    { x: 5, y: -2 },
+    { x: 4, y: -1 },
 ];
 
 export const SHAPES: TShapeList = [RECTANGLE, SQUARE, T_SHAPE];
